c34f9da99eb7672525de98a7abadf68f9ccc28da
exp eqp <expeqp@expnoMacBook-Air.local>
2024-11-27 12:18:46 +0900

    GithubCopilot-function-case1

--- python-test.py
+++ python-test.py
@@ -2,7 +2,7 @@
 import re
 
 # 正規表現ルールの宣言 Pythonのルールに則って宣言
-pattern = 'location="([^"]*)"'
+pattern = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 
 # マッチするか検証する文字列 特に変更せずOK
 subject = "test@example.com"
