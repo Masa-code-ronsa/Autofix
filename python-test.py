fb178f92a1eb37edee94ea75f1acfeeff3f4a7ec
exp eqp <expeqp@expnoMacBook-Air.local>
2024-11-27 11:18:10 +0900

    GithubCopilot-comment-case8

--- python-test.py
+++ python-test.py
@@ -2,7 +2,7 @@
 import re
 
 # 正規表現ルールの宣言 Pythonのルールに則って宣言
-pattern = ',(?=(?:[^"]*"[^"]*")*[^"]*$)\s*'
+pattern = '(?P<name>.+?)\s(?P<total_weight>\d+(\.\d+)?)\s?(?P<total_weight_unit>\S+)\s\((?P<unitWeight>\d+(\.\d+)?)\s?(?P<unitWeight_unit>\S+)\sx\s(?P<portion_no>\d+)\)'
 
 # マッチするか検証する文字列 特に変更せずOK
 subject = "test@example.com"
